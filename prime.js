dfe3400ea6bf24377694ae76a88ed3043fa258c4
Aditya Kumar <aditya.gecv21@gmail.com>
2023-02-19 12:49:52 +0530

    add prime function
    fixes: #2

--- prime.js
+++ prime.js
@@ -1,3 +1,14 @@
 function isPrime(n) {
-    
+    if (n <= 0) {
+        return false;
     }
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+        
+    }
+    return true;
+}
+let v=isPrime(process.argv[2]);
+console.log(v);
