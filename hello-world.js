d8cd186b82845ee207a779172c19d5111baf2d31
Aditya Kumar <aditya.gecv21@gmail.com>
2023-02-23 19:08:18 +0530

    If user don't give any input then show hello world fix :3

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,12 @@
-console.log("Hello "+process.argv[2]+" "+process.argv[3]);
+if (process.argv.length <= 2) {
+    console.log("hello world");
+}
+else if (process.argv.length > 2) {
+    let a = "";
+    for (let i = 2; i <= process.argv.length - 1; i++) {
+        a += process.argv[i];
+        a += " ";
+
+    }
+    console.log(a);
+}
